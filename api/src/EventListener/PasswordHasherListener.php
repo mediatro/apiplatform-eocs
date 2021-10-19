ef3b29d45ea13c04c3ccf33f35c6545683914bc7
Mediatro <mediatro.dev@gmail.com>
2021-10-19 23:49:01 +0300

    Auth init

---
+++ api/src/EventListener/PasswordHasherListener.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\EventListener;
+
+
+use App\Entity\User;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class PasswordHasherListener {
+
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordHasherInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function prePersist(User $user, LifecycleEventArgs $args): void {
+        $user->setPassword($this->passwordEncoder->hashPassword($user, $user->getPassword()));
+    }
+
+}
